bbde3c88e033d2563535e7fe286bf4bc289b8aa9
jdouglassmolloy <jdouglassmolloy@Schlopp.lan>
2019-10-19 14:44:37 +1030

    Add RsaSignature class under EncryptionValues module

---
+++ lib/cryppo/encryption_values/rsa_signature.rb
@@ -0,0 +1,23 @@
+module Cryppo
+    module EncryptionValues
+      class RsaSignature
+        attr_reader :signature
+
+        def initialize(signature, data)
+          @signature = signature
+          @data = data
+        end
+
+        def verify(data, public_key_string)
+          public_key = OpenSSL::PKey::RSA.new(public_key_string)
+          public_key.verify(OpenSSL::Digest::SHA256.new, signature, data)
+        end
+
+        def serialize()
+          "Sign.Rsa4096.#{Base64.urlsafe_encode64(signature)}.#{Base64.urlsafe_encode64(data)}"
+        end
+
+      end
+    end
+end
+  
