7304e0aedf63469e87682935d6ad54f420359dea
jdouglassmolloy <jdouglassmolloy@Schlopp.lan>
2019-10-21 15:01:08 +1030

    Change verify and serialize in RsaSignature to use instance variable @data

--- lib/cryppo/encryption_values/rsa_signature.rb
+++ lib/cryppo/encryption_values/rsa_signature.rb
@@ -1,20 +1,20 @@
 module Cryppo
     module EncryptionValues
       class RsaSignature
-        attr_reader :signature
+        attr_reader :signature, :data
 
         def initialize(signature, data)
           @signature = signature
           @data = data
         end
 
-        def verify(data, public_key_string)
+        def verify(public_key_string)
           public_key = OpenSSL::PKey::RSA.new(public_key_string)
-          public_key.verify(OpenSSL::Digest::SHA256.new, signature, data)
+          public_key.verify(OpenSSL::Digest::SHA256.new, @signature, @data)
         end
 
         def serialize()
-          "Sign.Rsa4096.#{Base64.urlsafe_encode64(signature)}.#{Base64.urlsafe_encode64(data)}"
+          "Sign.Rsa4096.#{Base64.urlsafe_encode64(@signature)}.#{Base64.urlsafe_encode64(@data)}"
         end
 
       end
